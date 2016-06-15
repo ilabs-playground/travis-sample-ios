226408eaeed52253970f6267876bd87c6df6b344
Steve Xu <steve@interactivelabs.co>
2016-06-15 01:02:30 -0700

    Try hockey upload

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -12,5 +12,22 @@ fi
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
+echo "Building $APP_NAME"
 xcodebuild -workspace "$APP_NAME.xcworkspace" -scheme "$APP_NAME" clean build
+
+echo "Packinage $APP_NAME"
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+echo "Upload to hockeyapp."
+curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
+  -F status="2" \
+  -F notify="0" \
+  -F notes="Automated Build" \
+  -F notes_type="0" \
+  -F ipa="@$OUTPUT_DIR/$APP_NAME.ipa" \
+  -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
+
+if [[ $? -ne 0 ]]; then
+  echo "Error: Fail uploading to HockeyApp"
+  exit 1
+fi
