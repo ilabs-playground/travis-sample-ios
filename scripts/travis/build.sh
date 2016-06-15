a781f29189ee21648aab1c1fd69a04894ee887dd
Steve Xu <steve@interactivelabs.co>
2016-06-15 01:21:00 -0700

    Hockey script with dSym

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -11,6 +11,7 @@ fi
 
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
+ARCHIVE_DIR="$OUTPUT_DIR/$APP_NAME.xcarchive"
 
 #echo "Building $APP_NAME"
 #xcodebuild -workspace "$APP_NAME.xcworkspace" -scheme "$APP_NAME" clean build
@@ -18,11 +19,14 @@ OUTPUTDIR="$PWD/build/Release-iphoneos"
 echo "Packaging $APP_NAME"
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
+zip -r -9 "$OUTPUT_DIR/$APP_NAME.app.dSYM.zip" "$ARCHIVE_DIR/dSYMs/$APP_NAME.app.dSYM"
+
 echo "Upload to hockeyapp."
 curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F status="2" \
   -F notify="0" \
   -F notes="Automated Build" \
+	-F dsym_path="@$OUTPUT_DIR/$APP_NAME.app.dSYM.zip" \
   -F notes_type="0" \
   -F ipa="@$OUTPUT_DIR/$APP_NAME.ipa" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
