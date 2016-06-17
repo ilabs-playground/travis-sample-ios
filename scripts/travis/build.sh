9366d87e9eeb3d1e939db48950fa7717701ffe8e
Steve Xu <steve@interactivelabs.co>
2016-06-17 00:51:12 -0700

    Try this

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -10,23 +10,22 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
 fi
 
 ARCHIVE_DIR="$PWD/build/archive/$APP_NAME.xcarchive"
-OUTPUT_DIR="$PWD/build/ipa"
+OUTPUT_DIR="$PWD/build/ipa/$APP_NAME"
 
 echo "Building $APP_NAME"
 xcodebuild CODE_SIGN_IDENTITY="$DEVELOPER_NAME" PROVISIONING_PROFILE="$PROVISION_UUID" -project "$APP_NAME.xcodeproj" -scheme "$APP_NAME" archive -archivePath $ARCHIVE_DIR 
 
 echo "Packaging $APP_NAME"
 xcodebuild -exportArchive -archivePath $ARCHIVE_DIR -exportPath $OUTPUT_DIR
-#xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
-zip -r -9 "$OUTPUT_DIR/$APP_NAME.app.dSYM.zip" "$ARCHIVE_DIR/dSYMs/$APP_NAME.app.dSYM"
+#zip -r -9 "$OUTPUT_DIR/$APP_NAME.app.dSYM.zip" "$ARCHIVE_DIR/dSYMs/$APP_NAME.app.dSYM"
 
 echo "Upload to hockeyapp."
 curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F status="2" \
   -F notify="0" \
   -F notes="Automated Build" \
-	-F dsym_path="@$OUTPUT_DIR/$APP_NAME.app.dSYM.zip" \
+#	-F dsym_path="@$OUTPUT_DIR/$APP_NAME.app.dSYM.zip" \
   -F notes_type="0" \
   -F ipa="@$OUTPUT_DIR/$APP_NAME.ipa" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
