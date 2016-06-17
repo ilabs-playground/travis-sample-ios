cdeb0818aa77e1bc30b83ef3c8493c8b09d260aa
Steve Xu <steve@interactivelabs.co>
2016-06-17 00:35:52 -0700

    Keychain tweak

--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -1,6 +1,11 @@
 security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
+security unlock-keychain -p travis ios-build.keychain
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+
 security import ./scripts/travis/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/travis/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/travis/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/travis/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
